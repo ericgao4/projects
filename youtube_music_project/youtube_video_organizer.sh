7b37ca0981192f55f25abb2a1635faf5c1c786cb
eric gao <ericgao@erics-MacBook-Pro.local>
2018-08-14 11:39:31 -0400

    started a spotify api project

--- youtube_music_project/youtube_video_organizer.sh
+++ youtube_music_project/youtube_video_organizer.sh
@@ -1,6 +1,6 @@
  # $1 is the url of the song/playlist/videos
- # $2 is the file path of the new music library
- # $3 is the name of the new music library
+ # $2 is the file path of the new video library
+ # $3 is the name of the new video library
  cd $2
  mkdir $3
  cd $3
